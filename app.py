0246fc8248c2b3b8801997f2aa56d259d4985033
madmax31230 <eble.michael@orange.fr>
2026-09-06 23:10:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def charger_gabarit_excel(fichier):
             
     return pd.DataFrame(donnees)
 
-fichier_source = "TEST_FEUILLE_DE_GARDE.xlsx"
+fichier_source = "TEST FEUILLE DE GARDE.xlsx"
 
 try:
     df = charger_gabarit_excel(fichier_source)
@@ -72,7 +72,7 @@ try:
         st.download_button(
             label="📥 Télécharger la feuille validée (Excel)", 
             data=output.getvalue(), 
-            file_name="Feuille_Garde_Modifiee.xlsx",
+            file_name="Feuille Garde Modifiee.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
 
