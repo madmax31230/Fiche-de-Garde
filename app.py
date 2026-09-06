93dbf1212231ef17f90a18e89cca80d265318c9f
madmax31230 <eble.michael@orange.fr>
2026-09-06 22:43:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,9 @@ st.title("🚒 Feuille de Garde")
 
 @st.cache_data
 def charger_gabarit(fichier):
-    with open(fichier, 'r', encoding='utf-8') as f:
+    # Remplacer 'utf-8' par 'latin-1' sur cette ligne :
+    with open(fichier, 'r', encoding='latin-1') as f:
         lignes = f.readlines()
-    donnees = []
-    agres = None
-    for ligne in lignes:
-        ligne = ligne.strip()
-        if not ligne or ligne == ",,": continue
-        elements = ligne.split(',')
-        
-        if elements[0] not in ["CA", "COND", "EQ", "CE B1", "EQ B1", "CE B2", "EQ B2", "OBS", "BILLET", "DE", "GARDE", "EQUIPE 2"] and len(elements) > 1 and elements[1].isdigit():
-            agres = elements[0]
-        elif agres and elements[0] in ["CA", "COND", "EQ", "CE B1", "EQ B1", "CE B2", "EQ B2", "OBS"]:
-            donnees.append({"Agrès": agres, "Fonction": elements[0], "Personnel": ""})
-    return pd.DataFrame(donnees)
-
 df = charger_gabarit("TEST_FEUILLE_DE_GARDE_BILLET.csv")
 
 st.write("Affectation des équipages :")
